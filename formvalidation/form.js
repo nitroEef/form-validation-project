2f6eab5163e47567a5c2b9f52c15169f712a3f52
nitroEef <mikebodetelli@gmail.com>
2024-03-14 12:07:11 +0100

    update

--- formvalidation/form.js
+++ formvalidation/form.js
@@ -1,5 +1,17 @@
-const form = document.getElementsById('form')
+const form = document.getElementById('form')
 const password1El  = document.getElementById('password1')
-const passwordTwo = document.getElementById('password')
-const message = document.getElementById('message')
-const button = document.getElementsByTagName('button')
+const password2El = document.getElementById('password2')
+const messageContainer = document.querySelector('.message-container')
+const message= document.getElementById('message')
+
+const processFormData = (e) => {
+   e.preventDefault();
+   console.log(e) //THE PAGE WONT BE RELOADING
+}
+form.addEventListener("submit", processFormData)
+
+let isValid = false;
+const validateForm = () => {
+    isValid = form.checkVisibility(  )
+    console.log(isValid)
+}

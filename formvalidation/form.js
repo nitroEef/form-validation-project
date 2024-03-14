6f8a9c74d13e385c20cdfbc2932b0908aff1b08f
nitroEef <mikebodetelli@gmail.com>
2024-03-14 11:03:06 +0100

    yuodD

---
+++ formvalidation/form.js
@@ -0,0 +1,5 @@
+const form = document.getElementsById('form')
+const password1El  = document.getElementById('password1')
+const passwordTwo = document.getElementById('password')
+const message = document.getElementById('message')
+const button = document.getElementsByTagName('button')
